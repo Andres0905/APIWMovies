ad394242d87c5da5686154881c5838539171616b
Andres0905 <152507221+Andres0905@users.noreply.github.com>
2025-11-23 13:31:17 -0500

    Add project files.

---
+++ APIWMovies/DAL/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using APIWMovies.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIWMovies.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        // Definimos el DbSet para la entidad Category
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ APIWMovies/DAL/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIWMovies.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        [Required] // indica que el campo es obligatorio
+
+        [Display(Name = "Nombre de la categoría")] // me sirve para mostrar un nombre amigable en las vistas
+        public string Name { get; set; }
+    }
+}
