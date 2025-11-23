a75c48ea90ecab5ce1c502a82c92a6952f114507
Andres0905 <152507221+Andres0905@users.noreply.github.com>
2025-11-23 14:09:37 -0500

    New CategoryRepository created

---
+++ APIWMovies/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using APIWMovies.DAL.Models;
+
+namespace APIWMovies.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>> GetCategoriesAsync(); // Obtener todas las categorías
+        Task<Category> GetCategoryAsync(int id); // Obtener una categoría por su ID
+        Task<bool> CategoryExistsByIdAsync(int id); // Verificar si una categoría existe por su ID
+        Task<bool> CategoryExistsByNameAsync(string name); // Verificar si una categoría existe por su nombre
+        Task<bool> CreateCategoryAsync(Category category); // Crear una nueva categoría
+        Task<bool> UpdateCategoryAsync(Category category); // Actualizar una categoría existente
+        Task<bool> DeleteCategoryAsync(int id); // Eliminar una categoría por su ID
+    }
+}
